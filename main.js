ea7a90a80b2669654c97f0aadc7a6cec6ca2f249
dkstjdwns114 <sungjoon256@gmail.com>
2020-12-22 20:34:16 +0900

    Update 구현1

--- main.js
+++ main.js
@@ -46,7 +46,6 @@ let app = http.createServer(function (request, response) {
             if (error2) {
               throw error2;
             }
-            console.log(topic[0].description);
             let title = topic[0].title;
             let description = topic[0].description;
             let list = template.list(topics);
@@ -110,31 +109,39 @@ let app = http.createServer(function (request, response) {
       );
     });
   } else if (pathname === "/update") {
-    fs.readdir("./data", function (error, filelist) {
-      let filteredId = path.parse(queryData.id).base;
-      fs.readFile(`data/${filteredId}`, "utf8", function (err, description) {
-        let title = queryData.id;
-        let list = template.list(filelist);
-        let html = template.HTML(
-          title,
-          list,
-          `
+    db.query("SELECT * FROM topic", function (error, topics) {
+      if (error) {
+        throw error;
+      }
+      db.query(
+        `SELECT * FROM topic WHERE id=?`,
+        [queryData.id],
+        function (error2, topic) {
+          if (error2) {
+            throw error2;
+          }
+          let list = template.list(topics);
+          let html = template.HTML(
+            topic[0].title,
+            list,
+            `
             <form action="/update_process" method="post">
-              <input type="hidden" name="id" value="${title}">
-              <p><input type="text" name="title" placeholder="title" value="${title}"></p>
+              <input type="hidden" name="id" value="${topic[0].id}">
+              <p><input type="text" name="title" placeholder="title" value="${topic[0].title}"></p>
               <p>
-                <textarea name="description" placeholder="description">${description}</textarea>
+                <textarea name="description" placeholder="description">${topic[0].description}</textarea>
               </p>
               <p>
                 <input type="submit">
               </p>
             </form>
             `,
-          `<a href="/create">create</a> <a href="/update?id=${title}">update</a>`
-        );
-        response.writeHead(200);
-        response.end(html);
-      });
+            `<a href="/create">create</a> <a href="/update?id=${topic[0].id}">update</a>`
+          );
+          response.writeHead(200);
+          response.end(html);
+        }
+      );
     });
   } else if (pathname === "/update_process") {
     let body = "";

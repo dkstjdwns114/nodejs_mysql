ca64e5a6e0277d08d62b86cfd92500779166c359
dkstjdwns114 <sungjoon256@gmail.com>
2020-12-22 20:03:17 +0900

    상세보기 구현

--- main.js
+++ main.js
@@ -35,29 +35,36 @@ let app = http.createServer(function (request, response) {
         response.end(html);
       });
     } else {
-      fs.readdir("./data", function (error, filelist) {
-        let filteredId = path.parse(queryData.id).base;
-        fs.readFile(`data/${filteredId}`, "utf8", function (err, description) {
-          let title = queryData.id;
-          let sanitizedTitle = sanitizeHtml(title);
-          let sanitizedDescription = sanitizeHtml(description, {
-            allowedTags: ["h1"]
-          });
-          let list = template.list(filelist);
-          let html = template.HTML(
-            sanitizedTitle,
-            list,
-            `<h2>${sanitizedTitle}</h2>${sanitizedDescription}`,
-            ` <a href="/create">create</a>
-                <a href="/update?id=${sanitizedTitle}">update</a>
+      db.query(`SELECT * FROM topic`, function (error, topics) {
+        if (error) {
+          throw error;
+        }
+        db.query(
+          `SELECT * FROM topic WHERE id=?`,
+          [queryData.id],
+          function (error2, topic) {
+            if (error2) {
+              throw error2;
+            }
+            console.log(topic[0].description);
+            let title = topic[0].title;
+            let description = topic[0].description;
+            let list = template.list(topics);
+            let html = template.HTML(
+              title,
+              list,
+              `<h2>${title}</h2>${description}`,
+              ` <a href="/create">create</a>
+                <a href="/update?id=${queryData.id}">update</a>
                 <form action="delete_process" method="post">
-                  <input type="hidden" name="id" value="${sanitizedTitle}">
+                  <input type="hidden" name="id" value="${queryData.id}">
                   <input type="submit" value="delete">
                 </form>`
-          );
-          response.writeHead(200);
-          response.end(html);
-        });
+            );
+            response.writeHead(200);
+            response.end(html);
+          }
+        );
       });
     }
   } else if (pathname === "/create") {

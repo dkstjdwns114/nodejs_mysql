58f9bf1c1d1956986bbd187ed47b300c527662a4
dkstjdwns114 <sungjoon256@gmail.com>
2020-12-22 21:41:32 +0900

    JOIN을 이용한 Create 기능 수정

--- main.js
+++ main.js
@@ -72,26 +72,31 @@ let app = http.createServer(function (request, response) {
     }
   } else if (pathname === "/create") {
     db.query(`SELECT * FROM topic`, function (error, topics) {
-      let title = "Create";
-      let list = template.list(topics);
-      let html = template.HTML(
-        title,
-        list,
-        `
-        <form action="/create_process" method="post">
-          <p><input type="text" name="title" placeholder="title"></p>
-          <p>
-            <textarea name="description" placeholder="description"></textarea>
-          </p>
-          <p>
-            <input type="submit">
-          </p>
-        </form>
-        `,
-        `<a href="/create">create</a>`
-      );
-      response.writeHead(200);
-      response.end(html);
+      db.query(`SELECT * FROM author`, function (error2, authors) {
+        let title = "Create";
+        let list = template.list(topics);
+        let html = template.HTML(
+          title,
+          list,
+          `
+          <form action="/create_process" method="post">
+            <p><input type="text" name="title" placeholder="title"></p>
+            <p>
+              <textarea name="description" placeholder="description"></textarea>
+            </p>
+            <p>
+              ${template.authorSelect(authors)}
+            </p>
+            <p>
+              <input type="submit">
+            </p>
+          </form>
+          `,
+          `<a href="/create">create</a>`
+        );
+        response.writeHead(200);
+        response.end(html);
+      });
     });
   } else if (pathname === "/create_process") {
     let body = "";
@@ -102,7 +107,7 @@ let app = http.createServer(function (request, response) {
       let post = qs.parse(body);
       db.query(
         `INSERT INTO topic (title, description, created, author_id) VALUES(?, ?, NOW(), ?)`,
-        [post.title, post.description, 1],
+        [post.title, post.description, post.author],
         function (error, result) {
           if (error) {
             throw error;
